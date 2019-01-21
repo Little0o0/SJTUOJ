29f9f614db9cf49431ba1251057a61812798ca84
Little0o0 <little0o0@protonmail.com>
2019-01-21 16:17:16 +0800

    AC

--- 4001/Main.java
+++ 4001/Main.java
@@ -1,21 +1,3 @@
-/*
- * 加法
-Description
-这道题和大家熟知的A+B的唯一区别是在输入信息中加入了很多空格什么的。
-
-Input Format
-输入仅有一行，原有的信息保证A+B的格式:例如 12324+3532 但可能在中间加入了一些无用的空格，原数据的相对位置不变 保证A,B为正整数，并且输入数据不找过1Kb 保证60%的数据没有多余空格
-
-Output Format
-输出一行: 如果答案大于10^8输出"Large"（不包括引号）,否则直接输出答案
-
-Sample Input
-1 +   2
-
-Sample Output
-3
- * 
- * */
 import java.math.BigInteger;
 import java.util.Scanner;
 
--- 4002/Main.java
+++ 4002/Main.java
@@ -1,25 +1,3 @@
-/*
- * 4002 SquareOfDigits
- * 
-Description
-给你一个矩形数组，获取其中的最大子正方形，该正方形满足四个角的值相同。
-
-Input Format
-第一行两个数n,m，表示矩阵的行数和列数 接下来n行，每行m个数，表示矩阵的元素。
-
-矩阵的元素都为数字（0..9），n,m <= 50
-
-Output Format
-输出一个数，表示符合条件的正方形内的元素个数
-
-Sample Input
-1 2
-1 1
-Sample Output
-1
- 
- * */
-
 import java.util.Scanner;
  
 public class Main {
